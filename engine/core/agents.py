cfbda8fa1f1dc7218263b6d7cf7f06d607bcf217
ReginaBDuarte <regina_duarte@outlook.pt>
2026-05-29 09:16:11 +0100

    update id to uuid in agent definitions

--- engine/core/agents.py
+++ engine/core/agents.py
@@ -4,51 +4,51 @@ from typing import Annotated, Literal
 
 
 class Region(BaseModel):
-    id: int
+    UUID: int
     name: str
     neighborhood: list[Region] = Field(default_factory=list)
     
 class Person(BaseModel):
-  id: int
-  age: int
-  parents: list[Person] = Field(default_factory=list)
-  children: list[Person] = Field(default_factory=list)
-  income: float
-  capital: float
-  education: int
-  nationality: str
+    UUID: int
+    age: int
+    parents: list[Person] = Field(default_factory=list)
+    children: list[Person] = Field(default_factory=list)
+    income: float
+    capital: float
+    education: int
+    nationality: str
 
 class Household(BaseModel):
-  id: int
-  members: list[Person] = Field(default_factory=list)
-  rent: float
-  contracts: list[Contracts] = Field(default_factory=list)
-  mortgages: list[Loan] = Field(default_factory=list)
-  living_house: House
-  houses: list[House] = Field(default_factory=list)
-  preference: Region
-  income: float
-  capital: float
+    UUID: int
+    members: list[Person] = Field(default_factory=list)
+    rent: float
+    contracts: list[Contracts] = Field(default_factory=list)
+    mortgages: list[Loan] = Field(default_factory=list)
+    living_house: House
+    houses: list[House] = Field(default_factory=list)
+    preference: Region
+    income: float
+    capital: float
   
   
 class House(BaseModel):
-  id: int
-  age: int
-  evaluation_price: float
-  selling_price: float
-  total_area: float
-  living_area: float
-  forSale: bool
-  forRent: bool
-  owner: Household | Constructor
-  quality: float
-  typology: Literal['T0', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6+']
-  size: float
-  location: Region
+    UUID: int
+    age: int
+    evaluation_price: float
+    selling_price: float
+    total_area: float
+    living_area: float
+    forSale: bool
+    forRent: bool
+    owner: Household | Constructor
+    quality: float
+    typology: Literal['T0', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6+']
+    size: float
+    location: Region
 
 
 class Contracts(BaseModel):
-    id: int
+    UUID: int
     house: House
     tenant: Household
     landlord: Household | Constructor
@@ -56,7 +56,7 @@ class Contracts(BaseModel):
     duration: int
 
 class Loan(BaseModel):
-    id: int
+    UUID: int
     house: House
     remaining_morgtage: float
     paid_mortgage: float
@@ -66,7 +66,7 @@ class Loan(BaseModel):
     tax_type: Literal['fixed', 'variable']
 
 class Constructor(BaseModel):
-    id: int
+    UUID: int
     capital: float
     loans: list[Loan] = Field(default_factory=list)
     pending_houses: list[House] = Field(default_factory=list)
